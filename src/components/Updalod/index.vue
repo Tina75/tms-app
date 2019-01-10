26e4dbdcd21199215419c63fb4a583077a9540ed
王军杰 <junjie.wang@ymm56.com>
2019-01-10 14:49:45 +0800

    公司设置UI

--- src/components/Updalod/index.vue
+++ src/components/Updalod/index.vue
@@ -4,7 +4,7 @@
       <li
         v-for="(item, index) in exceptionPhotos"
         :key="item.url"
-        class="cube-mt-50">
+        class="cube-mt-10 image-item">
         <img
           src="./images/icon-del-pic.png"
           class="pic-item-delete"
@@ -18,7 +18,7 @@
       </li>
       <li
         v-show="exceptionPhotos && exceptionPhotos.length < 6"
-        class="pic-item-add cube-mt-50"
+        class="cube-mt-10 image-item pic-item-add"
         @click="addImg">
         <icon-font
           name="yzg-hebingxingzhuang5"
@@ -46,10 +46,9 @@ export default {
           url: 'https://tms5566dev.oss-cn-hangzhou.aliyuncs.com/dolphinfile/order/483f7add-d29b-4602-97b4-9caf157649da/515643095740.77606.jpg?x-oss-process=image/resize,m_fill,h_100,w_100'
         }, {
           url: 'https://tms5566dev.oss-cn-hangzhou.aliyuncs.com/dolphinfile/order/483f7add-d29b-4602-97b4-9caf157649da/939238940611.8259.jpg?x-oss-process=image/resize,m_fill,h_100,w_100'
+        }, {
+          url: 'https://tms5566dev.oss-cn-hangzhou.aliyuncs.com/dolphinfile/order/483f7add-d29b-4602-97b4-9caf157649da/1204335750366.4255.png?x-oss-process=image/resize,m_fill,h_100,w_100'
         }
-        // , {
-        //   url: 'https://tms5566dev.oss-cn-hangzhou.aliyuncs.com/dolphinfile/order/483f7add-d29b-4602-97b4-9caf157649da/1204335750366.4255.png'
-        // }
       ]
     }
   },
@@ -94,12 +93,10 @@ export default {
 </script>
 <style lang='stylus' scoped>
 .content
-  padding 15px 10px
+  padding 15px 0 15px 0
   font-size 16px
   color #555555
   background white
-  margin-bottom 60px
-  margin-top: -40px;
   &-pic-box
     display flex
     display -webkit-flex
@@ -112,14 +109,17 @@ export default {
     .pic-item
       width 110px
       height 110px
+      border-radius 5px
   .pic-item-add
     background #F1F1F1
     line-height 110px
     text-align center
-    margin-top 10px
+    border-radius 5px
   .pic-item-delete
     position absolute
     width 24px
-    top -7px
-    left -5px
+    top -12px
+    right -10px
+.image-item
+  margin-bottom 60px
 </style>
