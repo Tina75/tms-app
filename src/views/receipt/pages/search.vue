c50dfba0269a755e11a16351711cef0b8ecc3c01
xubin <xpin_1509@163.com>
2019-01-11 14:35:38 +0800

    回单

---
+++ src/views/receipt/pages/search.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <form-item v-model="form.value" label="始发地"/>
+    <form-item v-model="form.value" label="目的地"/>
+    <form-item v-model="form.value" label="发货方名称"/>
+    <form-item v-model="form.value" label="订单号"/>
+    <form-item v-model="form.value" label="客户单号"/>
+    <form-item v-model="form.value" label="承运商"/>
+    <form-item v-model="form.value" label="司机"/>
+    <form-item v-model="form.value" label="司机手机号"/>
+    <form-item v-model="form.value" label="回收日期"/>
+    <form-item v-model="form.value" label="返厂日期"/>
+  </div>
+</template>
+<script>
+import { FormItem } from '@/components/Form'
+export default {
+  name: 'recept-search',
+  metaInfo: {
+    title: '回单搜索'
+  },
+  components: { FormItem },
+  data () {
+    return {
+      form: {
+        value: ''
+      }
+    }
+  },
+  methods: {
+    save () {
+      this.$router.push({ name: 'company' })
+    }
+  }
+}
+</script>
