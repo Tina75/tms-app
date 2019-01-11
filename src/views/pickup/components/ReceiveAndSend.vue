fee032035d5baec85239e7cb76bd7436b81576a3
chentianwei <chentw@ymm56.com>
2019-01-11 18:26:40 +0800

    提货

---
+++ src/views/pickup/components/ReceiveAndSend.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <detail-panel :title="'发货人'">
+      <detail-panel-item :label="'联系人'">
+        <p>刘亦菲</p>
+      </detail-panel-item>
+      <detail-panel-item :label="'联系方式'">
+        <p class="remix-content">
+          <span class="border-right-1px">18709870987</span>
+          <a>
+            联系TA
+            <icon-font
+              name="icon-ico_call"
+              :size="18"
+              color="#00A4BD"/>
+          </a>
+        </p>
+      </detail-panel-item>
+      <detail-panel-item :label="'发货地址'">
+        <p class="address">好嗨哟好嗨哟好嗨哟好嗨哟好嗨哟好嗨哟好嗨哟好嗨哟好嗨哟</p>
+      </detail-panel-item>
+    </detail-panel>
+    <detail-panel :title="'收货人'">
+      <detail-panel-item :label="'联系人'">
+        <p>刘亦菲</p>
+      </detail-panel-item>
+      <detail-panel-item :label="'联系方式'">
+        <p class="remix-content">
+          <span class="border-right-1px">18709870987</span>
+          <a>
+            联系TA
+            <icon-font
+              name="icon-ico_call"
+              :size="18"
+              color="#00A4BD"/>
+          </a>
+        </p>
+      </detail-panel-item>
+      <detail-panel-item :label="'发货地址'">
+        <p class="address">好嗨哟好嗨哟好嗨哟好嗨哟好嗨哟好嗨哟好嗨哟好嗨哟好嗨哟</p>
+      </detail-panel-item>
+    </detail-panel>
+  </div>
+</template>
+
+<script>
+// import { mapGetters, mapActions } from 'vuex'
+import detailPanel from '@/components/DetailPanel'
+import detailPanelItem from '@/components/DetailPanelItem'
+import IconFont from '@/components/Iconfont'
+
+export default {
+  name: 'pickupInfo',
+  components: { detailPanel, detailPanelItem, IconFont },
+  computed: {
+    // ...mapGetters(['BePickingData']),
+    options () {
+      return {
+        pullDownRefresh: true,
+        pullUpLoad: true,
+        scrollbar: true
+      }
+    }
+  },
+  methods: {
+    // ...mapActions(['getBePicking']),
+  }
+}
+</script>
+<style scoped lang="stylus">
+  .remix-content
+    display: flex
+    span
+      flex 1
+      text-align: right
+      padding-right: 10px;
+    a
+      padding-left: 10px;
+      color: #00a4bd;
+  .address
+    max-width 240px
+    text-align: left
+    float: right
+</style>
--- src/views/pickup/pages/order-detail.vue
+++ src/views/pickup/pages/order-detail.vue
@@ -1,45 +1,113 @@
 <template>
-  <div class="order-detail"/>
+  <div class="pickup-order-detail">
+    <cube-scroll-nav @change="changeHandler">
+      <div slot="prepend" class="status-block">
+        <h2>已提货</h2>
+        <p>2019-01-01 12:00</p>
+      </div>
+      <cube-scroll-nav-panel
+        v-for="(item, index) in pageData"
+        :key="index"
+        :label="item.name">
+        <ul>
+          <component :is="item.component"/>
+        </ul>
+      </cube-scroll-nav-panel>
+    </cube-scroll-nav>
+    <div class="handle-btns">
+      <a>调度</a>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import OrderBaseInfo from '../components/OrderBaseInfo'
+import ReceiveAndSend from '../components/ReceiveAndSend'
+import OrderCargoList from '../components/OrderCargoList'
+import CostDetail from '../components/CostDetail'
 
 export default {
   name: 'pickup-order-detail',
-  components: {},
   metaInfo: {
-    title: '订单详情'
+    title: '提货管理详情'
   },
+  components: { OrderBaseInfo, OrderCargoList, CostDetail, ReceiveAndSend },
   data () {
     return {
-      tabData: [
+      pageData: [
         {
-          label: '一',
-          anchor: 'text1'
+          name: '基本信息',
+          component: 'OrderBaseInfo'
         },
         {
-          label: '二',
-          anchor: 'text2'
+          name: '收发货人',
+          component: 'ReceiveAndSend'
         },
         {
-          label: '三',
-          anchor: 'text3'
+          name: '货物明细',
+          component: 'OrderCargoList'
         },
         {
-          label: '四',
-          anchor: 'text4'
+          name: '应收费用',
+          component: 'CostDetail'
         }
       ]
     }
   },
-  methods: {}
+  computed: {
+    ...mapGetters(['pickupDetail'])
+  },
+  methods: {
+    // ...mapActions(['getNews', 'clearNews']),
+    changeHandler (label) {
+      console.log('changed to:', label)
+    }
+  }
 }
 </script>
-<style lang="stylus">
-.order-detail
-  height: 100%
-  .content-box
-    height: 500px
-    background-color: #cccccc;
-    margin-bottom: 20px;
+<style lang="stylus" scoped>
+  .pickup-order-detail
+    height: 100%
+    background-color: #EFEFEF;
+    display: flex
+    flex-direction column
+    .cube-scroll-nav
+      flex: 1
+    .handle-btns
+      height: 45px
+      display: flex
+      a
+        flex: 1
+        background: #27A3BD;
+        text-align: center
+        height: 45px
+        line-height: 45px
+        font-size: 17px
+        color: #ffffff
+        &.light-btn
+          background: #32C4D3;
+    .status-block
+      height: 90px
+      background-color: #252A2F
+      padding: 25px 15px
+      h2
+        color: #ffffff;
+        font-size: 24px;
+        margin-bottom: 5px
+      p
+        color: #ffffff
+        font-size: 15px;
+    >>> .cube-scroll-content
+      width 100%
+      .cube-scroll-nav-bar-items
+        display: flex
+        .cube-scroll-nav-bar-item
+          flex 1
+          padding-top: 15px;
+          padding-bottom: 15px;
+    >>> .cube-scroll-nav-panel
+      .cube-sticky-ele
+        height 0
+        visibility: hidden
 </style>
