6d8881e1bfe2f7babf25fdff320938fb894efdb9
季康 <jik@ymm56.com>
2019-01-14 14:44:37 +0800

    新增消息列表

---
+++ src/views/message/store.js
@@ -0,0 +1,35 @@
+import Server from '@/libs/server'
+export default {
+  namespace: 'message',
+  state: {
+    msgDetail: {}
+  },
+  mutations: {
+    SET_MSG_DETAIL(state, payload) {
+      state.msgDetail = { ...payload }
+    }
+  },
+  actions: {
+    getMsgById: ({ commit }, id) => {
+      Server({
+        url: '/message/detail',
+        method: 'post',
+        data: { id }
+      }).then(({ data }) => {
+        commit('SET_MSG_DETAIL', data.data)
+      })
+    },
+    deleteMsgById: ({ commit }, id) => {
+      return Server({
+        url: '/message/del',
+        method: 'post',
+        data: { ids: [id] }
+      }).then(({ data }) => {
+        window.toast('删除成功')
+      })
+    }
+  },
+  getters: {
+    MsgDetail: state => state.msgDetail
+  }
+}
