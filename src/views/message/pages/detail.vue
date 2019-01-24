9893ef96fa5d90b002846b9bafa41565c04477f6
季康 <jik@ymm56.com>
2019-01-24 16:56:44 +0800

    修改消息详情

--- src/views/message/pages/detail.vue
+++ src/views/message/pages/detail.vue
@@ -1,6 +1,6 @@
 <!-- 消息详情 -->
 <template>
-  <div class="page msg ">
+  <div class="page msg">
     <p class="msg-title">{{MsgDetail.title}}</p>
     <span class="msg-time">{{MsgDetail.createTime | datetimeFormat}}</span>
     <pre class="msg-content" v-html="MsgDetail.content"/>
@@ -9,6 +9,8 @@
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import { setAppRightBtn } from '@/libs/bridgeUtil'
+
 export default {
   name: 'message-detail',
   metaInfo: { title: '消息' },
@@ -19,7 +21,12 @@ export default {
   beforeRouteEnter(to, from, next) {
     next(vm => {
       vm.getMsgById(to.params.id)
+      vm.setAppRightBtn([{ text: '删除', iconType: 'delete', action: () => { vm.deleteMsgById(to.params.id) } }])
     })
+  },
+  beforeRouteLeave(to, from, next) {
+    setAppRightBtn([{ text: '', action: () => { } }])
+    next()
   },
   methods: {
     ...mapActions('message', ['getMsgById', 'deleteMsgById'])
@@ -27,12 +34,14 @@ export default {
 }
 
 </script>
-<style lang='stylus' scoped>
+<style lang='stylus' >
 .msg
   width 375px
   padding 20px 15px
   background white
-  overflow hidden
+  overflowx-x hidden
+  overflow-y auto
+
   pre
     margin-top: 15px
     color: #666666
@@ -40,6 +49,7 @@ export default {
     line-height:20px;
     white-space: pre-wrap;
     word-wrap: break-word;
+
   &-title
     font-size 18px
     color #333333
@@ -55,6 +65,8 @@ export default {
     font-size 15px
     word-break break-all
     line-height 22px
-    margin-top 10px
     margin-bottom 10px
+    overflow hidden
+    img
+      width 100%
 </style>
