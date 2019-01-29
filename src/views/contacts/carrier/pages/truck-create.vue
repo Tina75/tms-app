444270b373eda62488b856ddf6a983f6e5505c7d
高润 <runner_1993@163.com>
2019-01-29 10:56:34 +0800

    fix: 承运商合作车辆驾驶证行驶证字段用反了

--- src/views/contacts/carrier/pages/truck-create.vue
+++ src/views/contacts/carrier/pages/truck-create.vue
@@ -57,8 +57,8 @@
 
       <card class="cube-mb-15" title="证件上传">
         <div class="uploadWrap">
-          <upload v-model="model.drivePhoto" label="点击上传行驶证"/>
-          <upload v-model="model.travelPhoto" label="点击上传道路运输证"/>
+          <upload v-model="model.travelPhoto" label="点击上传行驶证"/>
+          <upload v-model="model.drivePhoto" label="点击上传道路运输证"/>
         </div>
       </card>
 
