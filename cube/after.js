19270bf980a933dfe25ddfd4fc043c00486932f0
祁彦川 <yanchuan.qiyc@ymm56.com>
2019-01-30 14:47:37 +0800

    fix:兼容旧版本打包cube

--- cube/after.js
+++ cube/after.js
@@ -10,11 +10,13 @@ envs.forEach((filePath) => {
     fs.unlinkSync(filePath)
   }
 })
+// 兼容性
+clearAll(path.resolve(__dirname, '../public/cube'), true)
 clearPath(path.resolve(__dirname, './static/dev'))
 clearPath(path.resolve(__dirname, './static/prod'))
-copyFolder(path.resolve(__dirname, './static/dev'), path.resolve(__dirname, '../public/dev/cube'))
-copyFolder(path.resolve(__dirname, './static/prod'), path.resolve(__dirname, '../public/prod/cube'))
-clearAll(path.resolve(__dirname, './static'))
+move(path.resolve(__dirname, './static/dev'), path.resolve(__dirname, '../public/dev/cube'))
+move(path.resolve(__dirname, './static/prod'), path.resolve(__dirname, '../public/prod/cube'))
+clearAll(path.resolve(__dirname, './static'), true)
 console.info('---------完成--------')
 function clearPath(current, mode) {
   // 合并打包出的cube-index到cube-chunk末尾
@@ -22,33 +24,33 @@ function clearPath(current, mode) {
   const chunkPath = path.resolve(current, 'cube-chunk.js')
   if (fs.existsSync(indexPath) && fs.existsSync(chunkPath)) {
     var indexData = fs.readFileSync(indexPath, 'utf-8')
-    fs.appendFile(chunkPath, indexData, 'utf8', function(err) {
+    fs.appendFileSync(chunkPath, indexData, 'utf8', function(err) {
       if (err) {
         console.log(err)
       }
     })
+    console.info('--------合并cube--------')
   }
   choseRemove(current)
 }
 
-function choseRemove(dir, isDeep, allDel) {
+function choseRemove(dir, deepCount = 0) {
   let files = []
   if (fs.existsSync(dir)) {
     files = fs.readdirSync(dir)
     files.forEach((file, index) => {
       let curPath = dir + '/' + file
-      allDel = allDel || (['dev', 'prod', 'cube'].includes(file) && isDeep)
       if (fs.statSync(curPath).isDirectory()) {
-        if (!exculdeDir.includes(file) || allDel) {
-          choseRemove(curPath, true, allDel) //递归删除文件夹
+        if (!exculdeDir.includes(file) || deepCount > 0) {
+          choseRemove(curPath, deepCount + 1) //递归删除文件夹
         }
       } else {
-        if (!exculdeFile.includes(file) || allDel) {
+        if (!exculdeFile.includes(file) || deepCount > 0) {
           fs.unlinkSync(curPath) //删除文件
         }
       }
     })
-    if (isDeep) {
+    if (deepCount > 0) {
       fs.rmdirSync(dir)
     }
   }
@@ -72,27 +74,30 @@ function clearAll(dir, removed) {
   }
 }
 
-function copyFolder(from, to) {
-  // 复制文件夹到指定目录
+function move(from, to) {
   if (fs.existsSync(from)) {
-    let files = []
     if (fs.existsSync(to)) {
-      // 文件是否存在 如果不存在则创建
-      files = fs.readdirSync(from)
-      files.forEach(function(file, index) {
-        var targetPath = from + '/' + file
-        var toPath = to + '/' + file
-        if (fs.statSync(targetPath).isDirectory()) {
-          // 复制文件夹
-          copyFolder(targetPath, toPath)
-        } else {
-          // 拷贝文件
-          fs.copyFileSync(targetPath, toPath)
-        }
-      })
+      clearAll(to)
     } else {
       fs.mkdirSync(to)
-      copyFolder(from, to)
     }
+    copyFolder(from, to)
   }
+}
+
+function copyFolder(from, to) {
+  let files = []
+  files = fs.readdirSync(from)
+  files.forEach(function(file, index) {
+    var targetPath = from + '/' + file
+    var toPath = to + '/' + file
+    if (fs.statSync(targetPath).isDirectory()) {
+      // 复制文件夹
+      move(targetPath, toPath)
+    } else {
+      // 拷贝文件
+      fs.copyFileSync(targetPath, toPath)
+    }
+  })
+  clearAll(from)
 }
